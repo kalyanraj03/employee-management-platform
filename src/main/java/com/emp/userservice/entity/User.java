33f0db5b81704464c60138ade851da73d4fa21d4
kalyan <kalyanraj3003@gmail.com>
2026-08-06 11:29:58 +0530

    feat: added user entity and repository

--- src/main/java/com/emp/userservice/entity/User.java
+++ src/main/java/com/emp/userservice/entity/User.java
@@ -1,4 +1,44 @@
 package com.emp.userservice.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
